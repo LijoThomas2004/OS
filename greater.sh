8ed01be131aa21825d3b6c4de81b07e111d037e5
Lijo Thomas <166203370+LijoThomas2004@users.noreply.github.com>
2025-03-24 23:47:41 +0530

    Add files via upload

---
+++ greater.sh
@@ -0,0 +1,10 @@
+echo "Enter the first number:"
+read m
+echo "Enter the second number:"
+read n
+if (($m>$n))
+then
+echo "the largest is="$m
+else 
+echo "the largest is="$n
+fi
---
+++ sum.sh
@@ -0,0 +1,5 @@
+echo Enter thr first number:
+read num_1
+echo Enter the second number:
+read num_2
+echo sum=$(($num_1+$num_2))
