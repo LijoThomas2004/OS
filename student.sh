37f0a5cde1f705e0f59a6bd8299a8b47918af442
Lijo Thomas <166203370+LijoThomas2004@users.noreply.github.com>
2025-03-24 23:34:15 +0530

    Add files via upload

---
+++ student.sh
@@ -0,0 +1,16 @@
+echo Enter your Name:
+read name
+echo Enter your Semester:
+read sem
+echo Enter your Batch:
+read batch
+echo Enter your Rollno:
+read roll
+echo Enter your Register number:
+read reg
+
+echo Name:$name
+echo Rollno:$roll
+echo Batch:$batch
+echo Semester:$sem
+echo Register number:$reg
